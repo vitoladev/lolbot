8331b84ee218ee835848deb02d36d0c05d0be84e
vitoladev <asaptorvi@protonmail.com>
2021-05-01 19:57:56 -0300

    chore: delete unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PersonModule } from './modules/person/person.module';
+import { LeagueModule } from './modules/league/league.module';
+import { TeamModule } from './modules/team/team.module';
+import { NewsHeadlineModule } from './modules/news-headline/news-headline.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    PersonModule,
+    NewsHeadlineModule,
+    LeagueModule,
+    TeamModule,
+  ],
 })
 export class AppModule {}
